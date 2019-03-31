421b2c546458e7bd8f3d7360dd65e48bc62022ac
Mohamed Raslan <42682042+MohammedRaslan@users.noreply.github.com>
2019-03-31 20:11:24 +0200

    Add files via upload

---
+++ database/migrations/2019_03_31_163943_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('img',255);
+            $table->integer('like')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

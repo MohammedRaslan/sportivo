e751658cb710d068e9bec057370425d6c2d83043
mohamed mostafa <48805575+mohamed380@users.noreply.github.com>
2019-03-31 20:52:01 +0200

    Update 2019_03_31_163943_create_posts_table.php

--- database/migrations/2019_03_31_163943_create_posts_table.php
+++ database/migrations/2019_03_31_163943_create_posts_table.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+  
     public function up()
     {
         Schema::create('posts',function(Blueprint $table){
@@ -23,11 +19,7 @@ class CreatePostsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+ 
     public function down()
     {
         //

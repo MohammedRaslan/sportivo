880055168e9dd556f30b90d52c9c0df27c420477
Mohamed Raslan <42682042+MohammedRaslan@users.noreply.github.com>
2019-04-25 18:11:36 +0200

    Add files via upload

---
+++ app/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    //
+    protected $fillable = [
+        'body',
+        'user_id',
+        'commentable_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function post(){
+        return $this->belongsTo('App\Post');
+    }
+}

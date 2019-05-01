2784caf9cee7cc108eea794b0174006b5f32b0eb
Mohamed Raslan <42682042+MohammedRaslan@users.noreply.github.com>
2019-05-01 23:38:31 +0200

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session; 
 
 use Illuminate\Http\Request;
+
+use App\Contact;
+use App\Sidebar;
+use App\Post;
 
 class ContactController extends Controller
 {
     //
     public function index(){
-        return view('Frontview.contact');
+         $posts = new Post();
+        $posts = DB::table('posts')->select('id','body','img','author','created_at')->orderBy('id','desc')->get(); 
+        $singleton = Sidebar::getInstance();
+        $singleton = $posts;
+        return view('Frontview.contact')->with('singleton',$singleton);
     }
 }
